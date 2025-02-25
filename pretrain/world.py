415189fc5762738eee9b09461c0ada5df027f359
kangkang-yun <dengyunkang@hust.edu.cn>
2025-02-25 16:57:38 +0800

    Add files via upload

---
+++ pretrain/world.py
@@ -0,0 +1,38 @@
+import os
+from os.path import join
+import torch
+from enum import Enum
+from parse import parse_args
+import multiprocessing
+
+
+args = parse_args()
+
+config = {}
+# config['batch_size'] = 4096
+config['bpr_batch_size'] = args.bpr_batch
+config['latent_dim_rec'] = args.recdim
+config['lightGCN_n_layers']= args.layer
+config['dropout'] = args.dropout
+config['keep_prob']  = args.keepprob
+config['A_n_fold'] = args.a_fold
+config['test_u_batch_size'] = args.testbatch
+config['multicore'] = args.multicore
+config['lr'] = args.lr
+config['decay'] = args.decay
+config['pretrain'] = args.pretrain
+config['A_split'] = False
+config['bigdata'] = False
+
+GPU = torch.cuda.is_available()
+device = torch.device('cuda' if GPU else "cpu")
+CORES = multiprocessing.cpu_count() // 2
+seed = args.seed
+
+dataset = args.dataset
+sub_num = args.sub_num
+TRAIN_epochs = args.epochs
+topks = eval(args.topks)
+
+def cprint(words : str):
+    print(f"\033[0;30;43m{words}\033[0m")
